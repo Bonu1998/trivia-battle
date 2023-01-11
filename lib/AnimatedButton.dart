2af27a768aa958a4fe50f1dea693c94462989602
Manish <manish.suthar@flairlabs.com>
2023-01-11 14:08:24 +0530

    wip: button

--- lib/AnimatedButton.dart
+++ lib/AnimatedButton.dart
@@ -22,57 +22,113 @@ class AnimatedButton extends StatelessWidget {
     return InkWell(
       onTap: handler,
       child: Container(
-          height: (MediaQuery.of(context).size.height),
-          width: (MediaQuery.of(context).size.width),
-          // padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: colors,
-            ),
+        height: (MediaQuery.of(context).size.height),
+        width: (MediaQuery.of(context).size.width),
+        // padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: colors,
           ),
-          child: Row(
-            children: <Widget>[
-              SizedBox(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width * 0.05,
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+                top: 4,
+                left: 4,
                 child: Image(
                   image: AssetImage(
                       "assets/images/${icons['BUTTON']?['bubbles']}"),
-                  alignment: Alignment.topCenter,
-                ),
-              ),
-              Container(
-                // width: MediaQuery.of(context).size.width * 0.2,
-                padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
-                child: Image(
-                  image: AssetImage("assets/images/$logo"),
-                  width: MediaQuery.of(context).size.width * 0.1,
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.4,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      label,
-                      style: Theme.of(context).appBarTheme.titleTextStyle,
+                )),
+            Container(
+              alignment: Alignment.center,
+              padding:
+                  EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
+              height: (MediaQuery.of(context).size.height),
+              width: (MediaQuery.of(context).size.width),
+              child: Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.15,
+                    // padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                    child: Image(
+                      image: AssetImage("assets/images/$logo"),
+                      // width: MediaQuery.of(context).size.width * 0.1,
                     ),
-                    Text(
-                      tagLine,
-                      style: const TextStyle(
-                          color: Colors.yellow,
-                          fontWeight: FontWeight.w300,
-                          fontFamily: "Prompt-SemiBold"),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          label,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.05,
+                              fontFamily: "Prompt-SemiBold"),
+                        ),
+                        Text(
+                          tagLine,
+                          style: TextStyle(
+                              color: Colors.yellow,
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.03,
+                              fontFamily: "Prompt-SemiBold"),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              )
-            ],
-          )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+// child: Row(
+//             children: <Widget>[
+//               SizedBox(
+//                 height: MediaQuery.of(context).size.height,
+//                 width: MediaQuery.of(context).size.width * 0.05,
+//                 child: Image(
+//                   image: AssetImage(
+//                       "assets/images/${icons['BUTTON']?['bubbles']}"),
+//                   alignment: Alignment.topCenter,
+//                 ),
+//               ),
+//               Container(
+//                 // width: MediaQuery.of(context).size.width * 0.0,
+//                 padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+//                 child: Image(
+//                   image: AssetImage("assets/images/$logo"),
+//                   width: MediaQuery.of(context).size.width * 0.1,
+//                 ),
+//               ),
+//               SizedBox(
+//                 // width: MediaQuery.of(context).size.width * 0.4,
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Text(
+//                       label,
+//                       style: Theme.of(context).appBarTheme.titleTextStyle,
+//                     ),
+//                     Text(
+//                       tagLine,
+//                       style: const TextStyle(
+//                           color: Colors.yellow,
+//                           fontWeight: FontWeight.w300,
+//                           fontFamily: "Prompt-SemiBold"),
+//                     ),
+//                   ],
+//                 ),
+//               )
+//             ],
+//           )
